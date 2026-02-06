3ffca1d55d2681acc49e5b6340630f5a120dedd3
ranjan <ranjan.rt@assettl.com>
2026-02-06 10:18:31 +0530

    dashboard component updated

--- src/app/pages/dashboards/dashboard2/dashboard2.component.ts
+++ src/app/pages/dashboards/dashboard2/dashboard2.component.ts
@@ -2,36 +2,22 @@ import { Component } from '@angular/core';
 import { TablerIconsModule } from 'angular-tabler-icons';
 import { AppRevenueProductComponent } from 'src/app/components/dashboard1/revenue-product/revenue-product.component';
 import { AppAnnualProfitComponent } from 'src/app/components/dashboard2/annual-profit/annual-profit.component';
-import { AppBlogCardComponent } from 'src/app/components/dashboard2/blog-card/blog-card.component';
 import { CurrentMonthComponent } from 'src/app/components/dashboard2/current-month/current-month.component';
-import { AppDailyActivitiesComponent } from 'src/app/components/dashboard2/daily-activities/daily-activities.component';
 import { LastMonthComponent } from 'src/app/components/dashboard2/last-month/last-month.component';
 import { MonthEndComponent } from 'src/app/components/dashboard2/month-end/month-end.component';
-import { AppNewCustomersComponent } from 'src/app/components/dashboard2/new-customers/new-customers.component';
 import { AppRevenueForecastComponent } from 'src/app/components/dashboard2/revenue-forecast/revenue-forecast.component';
 import { AppSalesLocationsComponent } from 'src/app/components/dashboard2/sales-locations/sales-locations.component';
 import { AppSubscriptionsComponent } from 'src/app/components/dashboard2/subscriptions/subscriptions.component';
-import { AppTotalIncomeComponent } from 'src/app/components/dashboard2/total-income/total-income.component';
-import { AppWeeklyScheduleComponent } from 'src/app/components/dashboard2/weekly-schedules/weekly-schedules.component';
-import { AppWeeklyStatsComponent } from 'src/app/components/dashboard2/weekly-stats/weekly-stats.component';
 
 @Component({
   selector: 'app-dashboard2',
   standalone: true,
   imports: [
     TablerIconsModule,
-
     AppSubscriptionsComponent,
-
     AppRevenueForecastComponent,
     AppRevenueProductComponent,
     AppAnnualProfitComponent,
-    AppWeeklyScheduleComponent,
-    AppNewCustomersComponent,
-    AppTotalIncomeComponent,
-    AppWeeklyStatsComponent,
-    AppDailyActivitiesComponent,
-    AppBlogCardComponent,
     AppSalesLocationsComponent,
     LastMonthComponent,
     CurrentMonthComponent,
--- src/app/pages/dashboards/dashboards.routes.ts
+++ src/app/pages/dashboards/dashboards.routes.ts
@@ -1,23 +1,10 @@
 import { Routes } from '@angular/router';
-
-// dashboards
-import { AppDashboard1Component } from './dashboard1/dashboard1.component';
 import { AppDashboard2Component } from './dashboard2/dashboard2.component';
-import { AppDashboard3Component } from './dashboard3/dashboard3.component';
 
 export const DashboardsRoutes: Routes = [
   {
     path: '',
     children: [
-      // {
-      //   path: 'dashboard1',
-      //   component: AppDashboard1Component,
-      //   data: {
-      //     title: 'Dashboard 1',
-        
-      //     breadcrumb: false,
-      //   },
-      // },
       {
         path: 'dashboard2',
         component: AppDashboard2Component,
@@ -26,14 +13,6 @@ export const DashboardsRoutes: Routes = [
           breadcrumb: false,
         },
       },
-      // {
-      //   path: 'dashboard3',
-      //   component: AppDashboard3Component,
-      //   data: {
-      //     title: 'Dashboard 3',
-      //     breadcrumb: false,
-      //   },
-      // },
     ],
   },
 ];
